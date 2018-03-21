0e1adbacb4dc7f0b7bedd5319068e6ae7e76bcbd
Thibaut DEPOMMIER <thibaut.depommier@etudiant.univ-lille1.fr>
2018-03-21 16:06:09 +0100

    .

--- serveur_REST/common/src/main/java/fr/iutinfo/skeleton/common/dto/UserDto.java
+++ serveur_REST/common/src/main/java/fr/iutinfo/skeleton/common/dto/UserDto.java
@@ -3,23 +3,13 @@ package fr.iutinfo.skeleton.common.dto;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.security.Principal;
-
 public class UserDto {
     final static Logger logger = LoggerFactory.getLogger(UserDto.class);
-    private String name;
-    private String alias;
+    private String user;
+    private String fname;
+	private String lname;
     private int id = 0;
-    private String email;
     private String password;
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
 
     public int getId() {
         return id;
@@ -29,28 +19,36 @@ public class UserDto {
         this.id = id;
     }
 
-    public String getName() {
-        return name;
+    public String getUser() {
+		return user;
+	}
+
+	public void setUser(String user) {
+		this.user = user;
+	}
+
+	public String getfname() {
+        return fname;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public void setfname(String fname) {
+        this.fname = fname;
     }
 
+
+    public String getlname() {
+        return lname;
+    }
+
+    public void setlname(String lname) {
+        this.lname = lname;
+    }
     public String getPassword() {
         return this.password;
     }
 
     public void setPassword(String password) {
         this.password = password;
-    }
-
-    public String getAlias() {
-        return alias;
-    }
-
-    public void setAlias(String alias) {
-        this.alias = alias;
     }
 
 }
