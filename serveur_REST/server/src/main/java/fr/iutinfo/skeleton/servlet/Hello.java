67697d4d9a8d5891979bb5bd6edb6666451bf4aa
Brian MULIER <brian.mulier@etudiant.univ-lille1.fr>
2018-03-21 15:07:46 +0100

    gitIUT to gitHUB

--- serveur_REST/server/src/main/java/fr/iutinfo/skeleton/servlet/Hello.java
+++ serveur_REST/server/src/main/java/fr/iutinfo/skeleton/servlet/Hello.java
@@ -12,7 +12,12 @@ import java.io.PrintWriter;
 @WebServlet(name = "testServlet", urlPatterns = {"/hello"}, initParams = {@WebInitParam(name = "simpleParam", value = "paramValue")})
 public class Hello extends HttpServlet {
 
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    /**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         PrintWriter out = response.getWriter();
         String simpleParam = getServletConfig().getInitParameter("simpleParam");
         out.println("Hello World " + simpleParam);
@@ -22,5 +27,9 @@ public class Hello extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         doGet(request, response);
     }
+    
+    public static void main(String[] args) {
+		System.out.println("test : Hello World !");
+	}
 
 }
