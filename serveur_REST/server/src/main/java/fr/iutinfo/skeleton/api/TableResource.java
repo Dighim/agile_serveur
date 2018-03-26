b6711dc9a2496a70f9f3af463f9b68f12cdb2843
Brian MULIER <brian.mulier@etudiant.univ-lille1.fr>
2018-03-26 14:33:45 +0200

    Etat

--- serveur_REST/server/src/main/java/fr/iutinfo/skeleton/api/TableResource.java
+++ serveur_REST/server/src/main/java/fr/iutinfo/skeleton/api/TableResource.java
@@ -12,6 +12,7 @@ import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -45,6 +46,7 @@ public class TableResource {
 			t.setNbPers(1);
 			t.setPublique(false);
 			t.setCrea(1);
+			t.setEtat(1);
 			t.setDate(LocalDateTime.now().toString());
 			dao.insert(t); 
 		}
@@ -81,6 +83,12 @@ public class TableResource {
 		dto.setIdTable(id);
 		return dto;
 	}
+    
+    @PUT
+    @Path("{idTable}/etat/{etat}")
+    public void setEtat(@PathParam("idTable") int idTable, @PathParam("etat") int etat) {
+    	dao.updateState(etat, idTable);
+    }
     
     @GET
 	@Path("/{idTable}")
